eb12fbfc1f347eb382f9a7648ad7c7cba3768f51
DaveRubin <dudrubin@gmail.com>
2017-01-09 23:01:00 +0200

    implemented some more methods

--- Ex02/xv6/semaphores.c
+++ Ex02/xv6/semaphores.c
@@ -2,14 +2,19 @@
 
 #include "types.h"
 #include "param.h"
-#include "memlayout.h"
-#include "mmu.h"
 #include "defs.h"
-#include "x86.h"
-#include "elf.h"
 #include "proc.h"
-#include "proc.c"
+#include "defs.h"
+#include "Defs.h"
 #include "semaphores.h"
+//
+//
+//#include "types.h"
+//#include "param.h"
+//#include "proc.h"
+//#include "defs.h"
+//#include "Defs.h"
+//#include "semaphores.h"
 
 struct {
     struct spinlock gslock; //global semaphore lock
@@ -23,7 +28,11 @@ void seminit(void) {
 int sem_open(char *name, int init, int maxVal) {
 
     cprintf("Sem open %s %d %d\n", name, init, maxVal);
-    if (strlen(name) > 6)
+    if (name == 0 ||
+        strlen(name) > 6 ||
+        init < 0 ||
+        maxVal < 0 ||
+        maxVal < init)
         return -1;
 
     acquire(&stable.gslock);
@@ -35,7 +44,7 @@ int sem_open(char *name, int init, int maxVal) {
     for (s = stable.sem; s < stable.sem + NSEM; s++) {
 
         cprintf("checking index %d with reference\n", i, s->ref);
-        if (s->ref != 0 && strncmp(s->name, name, 6) == 0) {
+        if (s->status == SEM_ALIVE && strncmp(s->name, name, 6) == 0) {
             cprintf("same name \n");
             //found
             s->ref++;
@@ -47,19 +56,9 @@ int sem_open(char *name, int init, int maxVal) {
                 s->ref--;
             }
             cprintf("i = %d, sf ref %d, maxref %d\n", i, s->ref, s->maxVal);
-            if (s->ref > s->maxVal) {
-                release(&stable.gslock);
-                while(s->ref > s->maxVal ) {
-                    cprintf("Sleep \n");
-                    acquire(&s->sslock);
-                    sleep(s, &s->sslock);
-                }
-                return i;
-            }
-            else {
-                release(&stable.gslock);
-                return i;
-            }
+
+            release(&stable.gslock);
+            return i;
         } else if (freeIndex == -1) {
             //save the first free index in the table
             freeIndex = i;
@@ -75,9 +74,12 @@ int sem_open(char *name, int init, int maxVal) {
         //found free spot
         safestrcpy(name, s->name, sizeof(name));
         s->ref = 1;
+        s->val = init;
         s->maxVal = maxVal;
+        s->status = SEM_ALIVE;
         initlock(&s->sslock, name);
         i = sem_alloc(s);
+
         if (i < 0) {
             //free semaphore
             s->ref--;
@@ -92,49 +94,141 @@ int sem_open(char *name, int init, int maxVal) {
 }
 
 int sem_close(int sd) {
+    if (sd < 0 || sd >= NSEM)
+        return  -1;
+
     cprintf("sem_close\n");
-    if (sd<0 || sd > NSEM)
-        return -1;
+    acquire(&stable.gslock);
 
     //get semaphore from process
     struct semaphore *s = proc->osem[sd];
-    if (s == 0)
+    if (s->status == SEM_DEAD) {
+        release(&stable.gslock);
         return -1;
+    }
 
     //clear the reference from the osem
     proc->osem[sd] = 0;
-
-    //reduce ref count, and close if ref reached 0
-    s->ref--;
-    if (s->ref <= s->maxVal ){
-        cprintf("Wakeup!!!");
-        wakeup(s);
-    }
+    --s->ref;
+    release(&stable.gslock);
     return 0;
 }
 
+//--
 int sem_wait(int sd) {
+    if (sd < 0 || sd >= NSEM)
+        return  -1;
     cprintf("sem_wait\n");
+    acquire(&stable.gslock);
+
+    struct semaphore *s = proc->osem[sd];
+
+    while (s->val <= 0) {
+        sleep(s, &stable.gslock);
+    }
+
+    s->val--;
+
+    release(&stable.gslock);
     return 0;
 }
 
 int sem_try_wait(int sd) {
     cprintf("sem_try_wait\n");
-    return 0;
+    if (sd < 0 || sd >= NSEM)
+        return  -1;
+    acquire(&stable.gslock);
+
+    struct semaphore *s = proc->osem[sd];
+    if (s->status == SEM_DEAD) {
+        release(&stable.gslock);
+        return -1;
+    }
+
+    if (s->val > 0) {
+        s->val--;
+        return 0;
+    }
+
+    release(&stable.gslock);
+    return -1;
 }
 
+//++
 int sem_signal(int sd) {
+    if (sd < 0 || sd >= NSEM)
+        return  -1;
+    acquire(&stable.gslock);
+    struct semaphore *s = proc->osem[sd];
+    //if sd isn't an open semaphore return -1
+    if (s->status == SEM_DEAD) {
+        release(&stable.gslock);
+        return -1;
+    }
+
+    //if val at maxVal then return -2
+    if (s->val == s->maxVal) {
+        release(&stable.gslock);
+        return -2;
+    }
+
+    s->val++;
+    if (s->val > 0) {
+        wakeup(s);
+    }
+
     cprintf("sem_signal\n");
+    release(&stable.gslock);
     return 0;
 }
 
+/**
+ * gets sd and returns the value and maxVlue of a sempahore via given pointers
+ * return 0 if everything went smooth, and -1 for error
+ */
 int sem_gat_value(int sd, int *val, int *maxVal) {
+    if (sd < 0 || sd >= NSEM)
+        return  -1;
     cprintf("sem_gat_value\n");
+    acquire(&stable.gslock);
+
+    struct semaphore *s = proc->osem[sd];
+    if (s->status == SEM_DEAD) {
+        release(&stable.gslock);
+        return -1;
+    }
+
+    *val = s->val;
+    *maxVal = s->maxVal;
+    release(&stable.gslock);
     return 0;
 }
+/**
+ * closes a semaphore for good!
+ * also erases all osem references
+ */
+int sem_unlink(int sd) {
+    if (sd < 0 || sd >= NSEM)
+        return  -1;
 
-int sem_unlink(int fd) {
     cprintf("sem_unlink\n");
+    acquire(&stable.gslock);
+
+    struct semaphore *s = proc->osem[sd];
+    if (s->status == SEM_DEAD) {
+        release(&stable.gslock);
+        return -1;
+    }
+
+    //run through all processes and remove sd from them
+    while (s->ref > 0 ) {
+        sleep(s,&stable.gslock);
+    }
+
+    //now that no process have reference to this lock... clear
+    s->status = SEM_DEAD;
+
+    release(&stable.gslock);
     return 0;
 }
 
--- Ex02/xv6/semaphores.h
+++ Ex02/xv6/semaphores.h
@@ -1,10 +1,12 @@
 #include "spinlock.h"
-
+enum STATUS {SEM_DEAD,SEM_ALIVE};
 struct semaphore {
     struct spinlock sslock; // single semaphore lock
     char name[6];
     int ref;
+    int val;
     int maxVal;
+    enum STATUS status;
 };
 
 void seminit(void);
